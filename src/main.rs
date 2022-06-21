575934910134948a88cee57c6c45b77b805d48ba
Eric Kidd <git@randomhacks.net>
2022-06-21 17:53:40 -0400

    Allow running repeatedly (for summoners)

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,10 @@ struct Args {
     )]
     color: ColorArg,
 
+    /// Repeat all our rolls this many times.
+    #[structopt(long = "repeat", short = 'r', default_value = "1")]
+    repeat: u32,
+
     /// Expressions of the form "2d6+3" or "d20".
     dice_exprs: Vec<String>,
 }
@@ -54,17 +58,33 @@ fn main() {
 
 /// Does the actual work.
 fn run(args: &Args) -> Result<(), ProgramDiagnostics> {
-    let mut rng = ThreadRng::default();
-    let mut writer = StandardStream::stdout(args.color.into());
+    // Parse all our input programs.
+    let mut programs = vec![];
     for (idx, dice_expr) in args.dice_exprs.iter().enumerate() {
         let file_name = FileName::Arg(idx + 1);
-        let program = Program::parse(file_name, dice_expr)?;
-        let rolled = program.roll_all(&mut rng);
-        rolled.evaluate_and_pretty_format(&mut writer)?;
-        writer
-            .write(b"\n")
-            .map_err(ProgramDiagnostics::from_error)?;
+        programs.push(Program::parse(file_name, dice_expr)?);
     }
+
+    // Run all of our programs `args.repeat` times.
+    let mut rng = ThreadRng::default();
+    let mut writer = StandardStream::stdout(args.color.into());
+    for i in 1..=args.repeat {
+        // Print a group header.
+        if args.repeat > 1 {
+            writeln!(writer, "=== Group {i}").map_err(ProgramDiagnostics::from_error)?;
+        }
+
+        // Run each program and print the output.
+        for program in &programs {
+            let rolled = program.roll_all(&mut rng);
+            rolled.evaluate_and_pretty_format(&mut writer)?;
+            writer
+                .write(b"\n")
+                .map_err(ProgramDiagnostics::from_error)?;
+        }
+    }
+
+    // Flush our output.
     writer.flush().map_err(ProgramDiagnostics::from_error)?;
     Ok(())
 }
